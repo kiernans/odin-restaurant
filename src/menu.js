b03bdfc07981e5774386be8a167bccc6a3483094
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-06-22 19:18:33 -0700

    Create button listener, displays page

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,8 @@
+import vegan from './images/1.jpg';
+import pork from './images/2.jpg';
+import barbacoa from './images/3.jpg';
+import chicken from './images/4.jpg';
+
 const Menu = (() => {
     
 })();
