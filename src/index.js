dd91fb596afbd0125d6c5353551e4aa340d49c2f
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-06-22 18:22:49 -0700

    Rename image files, allow for images in webpack.config, create removeDOM function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import './style.css';
 import MainPage from './home';
 
 MainPage.displayPage();
+
+const removeDOM = () => {
+    const content = document.querySelector('.content');
+
+    while(content.lastChild) {
+        content.remove(lastChild);
+    }
+};
